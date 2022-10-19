6254571e8f4c4cdccd5019789f270db4f7019b5f
Adam Yao <adamyao3240@gmail.com>
2022-10-18 23:53:46 -0400

    Login View and Registration View

--- JacketStash_PROD/ContentView.swift
+++ JacketStash_PROD/ContentView.swift
@@ -9,22 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showMenu = false
-//    @EnvironmentObject var viewModel: AuthViewModel
+    @EnvironmentObject var viewModel: AuthViewModel
     
     var body: some View {
-        //        Group {
-        //
-        //            if viewModel.userSession == nil { // user logged in
-        //                LoginView()
-        //            } else {
-        //                //user not logged in
-        //                mainInterfaceView
-        //            }
-        //
-        //
-        //        }
-        //    }
-        Text("Hello")
+        Group {
+            
+            if viewModel.userSession == nil { // user logged in
+                LoginView()
+            } else {
+                //user not logged in
+                mainInterfaceView
+            }
+            
+            
+        }
     }
 }
 
@@ -33,3 +31,47 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+extension ContentView {
+    var mainInterfaceView: some View {
+        ZStack (alignment: .topLeading) {
+            MainTabView()
+                .navigationBarHidden(showMenu)
+            
+            if showMenu {
+                ZStack {
+                    Color(.black).opacity(showMenu ? 0.25 : 0.0)
+                }.onTapGesture {
+                    withAnimation(.easeInOut){
+                        showMenu = false
+                    }
+                }
+                .ignoresSafeArea()
+            }
+            
+            SideMenuView()
+                .frame(width: 300)
+                .offset(x:showMenu ? 0 : -300, y: 0)
+                .background(showMenu ? Color.white : Color.clear)
+        }
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    withAnimation(.easeInOut) {
+                        showMenu.toggle()
+                    }
+                } label: {
+                    Circle ()
+                        .frame(width:32, height: 32)
+                }
+
+            }
+        }
+        .onAppear {
+            showMenu = false
+        }
+    }
+}
+
