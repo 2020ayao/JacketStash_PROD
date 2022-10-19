6dcdddad1330c19f6b23c6464bef42c4d151a461
Adam Yao <adamyao3240@gmail.com>
2022-10-18 23:39:32 -0400

    Initial commit

---
+++ JacketStash_PROD/JacketStash_PRODApp.swift
@@ -0,0 +1,17 @@
+//
+//  JacketStash_PRODApp.swift
+//  JacketStash_PROD
+//
+//  Created by Adam Yao on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct JacketStash_PRODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
