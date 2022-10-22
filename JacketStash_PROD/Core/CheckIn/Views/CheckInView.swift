ce16f8558c85cf63c106c5f2d435b682a61b8c12
Adam Yao <adamyao3240@gmail.com>
2022-10-21 22:34:23 -0400

    Button animation FINALIZED

--- JacketStash_PROD/Core/CheckIn/Views/CheckInView.swift
+++ JacketStash_PROD/Core/CheckIn/Views/CheckInView.swift
@@ -18,7 +18,6 @@ struct CheckInView: View {
     @State var show = false
     
     @State private var isPressed = false
-    @State private var isPressed1 = false
     
     
     var body: some View {
@@ -27,52 +26,7 @@ struct CheckInView: View {
             
             Spacer()
             
-            
-            
-            ZStack {
-                
-                Circle()
-                    .fill(Color.black)
-                    .frame(width: 100, height: 100)
-                    .scaleEffect(isPressed1 ? 2 : 1)
-                    .animation(.easeInOut(duration: 2))
-                
-                
-                Button(action: {
-                    print("hello")
-                }, label: {
-//                    Text("Check In")
-//                        .foregroundColor(Color.white)
-                })
-                .onLongPressGesture(perform: {
-                    print("hello")
-                })
-                
-                //.foregroundColor(.white)
-                .frame(width: 100, height: 100)
-                .background(Color(.systemBlue))
-                .mask(Circle())
-                .scaleEffect(isPressed ? 2 : 1)
-                .pressEvents {
-                    withAnimation(.easeInOut(duration: 2.75)) {
-                        isPressed = true
-                        isPressed1 = true
-                        
-                        
-                    }
-                } onRelease: {
-                    withAnimation(.easeInOut(duration: 0.5)) {
-                        isPressed = false
-                        isPressed1 = false
-                    }
-                }
-                Text("Check In")
-                    .foregroundColor(.white)
-            }
-            
-            .padding(.bottom, 60)
-            
-            
+            checkInButton
         }
         .ignoresSafeArea()
     }
@@ -87,65 +41,48 @@ struct CheckInView_Previews: PreviewProvider {
 
 extension CheckInView {
     
-//    var swipeToConfirmButton: some View {
-//        ZStack {
-//            Capsule()
-//                .frame(width: trackSize.width, height: trackSize.height)
-//                .foregroundColor(Color.black).blendMode(.overlay).opacity(0.5)
-//
-//
-//            Text("Swipe to confirm Check In")
-//                .font(.caption)
-//                .foregroundColor(Color.white)
-//                .offset(x:30, y:0)
-//
-//            ZStack {
-//                Capsule()
-//                    .frame(wdith: thumbSize.width, height: thumbSize.height)
-//                    .foregroundColor(Color.white)
-//
-//                Image(systemName: "arrow.right")
-//                    .foregroundColor(Color.black)
-//            }
-//            .offset(x:-(trackSize.width/2 - thumbSize.width/2 - (dragOffset.width)), y:0)
-//
-//        }
-//    }
-    
-    var longPress: some Gesture {
-            LongPressGesture(minimumDuration: 3)
-                .updating($isDetectingLongPress) { currentState, gestureState,
-                        transaction in
-                    gestureState = currentState
-                    transaction.animation = Animation.easeIn(duration: 2.0)
-                }
-                .onEnded { finished in
-                    self.completedLongPress = finished
-                }
-        }
-    
-    var longPressColorButton : some View {
-        Button {
-            print("pressing")
-        } label: {
-            Circle()
-                .fill(self.isDetectingLongPress ? Color.red : (self.completedLongPress ? Color.green : Color.blue))
-                .frame(width: 100, height: 100, alignment: .center)
-                .foregroundColor(Color(.systemBlue))
-                .gesture(longPress)
-        }
-    }
     var checkInButton : some View {
-        Button {
-            isChecked.toggle()
-        } label: {
-            HStack {
-                Image(systemName: isChecked ? "checkmark.square": "square")
-                Text("I understand")
+        ZStack {
+            Circle()
+                .fill(Color(.systemBlue))
+                .frame(width: 100, height: 100)
+                .scaleEffect(isPressed ? 2 : 0.99)
+                .animation(.easeIn(duration: 1.5), value: isPressed)
+                //.animation(.easeOut(duration: 0.5), value: isPressed)
+            
+            Circle()
+                .fill(Color(.white))
+                .frame(width: 100, height: 100)
+                .scaleEffect(isPressed ? 1.99 : 0.5)
+                .animation(.easeOut(duration: 3), value: isPressed)
+            
+            Button(action: {
+                print("hello")
+            }, label: {})
+            .onLongPressGesture(perform: {
+                print("hello")
+            })
+            .frame(width: 100, height: 100)
+            .background(Color(.systemBlue))
+            .mask(Circle())
+            
+            .pressEvents {
+                withAnimation(.easeIn(duration: 2.75)) {
+                    isPressed = true
+                }
+            
+            } onRelease: {
+                withAnimation(.easeOut(duration: 0.5)) {
+                    isPressed = false
+                }
             }
-            .padding()
-            .shadow(color: .gray.opacity(0.5), radius: 10, x:0, y:0)
+            Text("Check In")
+                .foregroundColor(.white)
         }
+        
+        .padding(.bottom, 120)
+        
+        
     }
 }
 
