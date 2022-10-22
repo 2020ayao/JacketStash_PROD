05c08413cfc4d3bae178725280cb7a48ae2c7c55
Adam Yao <adamyao3240@gmail.com>
2022-10-22 00:13:07 -0400

    push to origin

--- JacketStash_PROD/Core/CheckIn/Views/CheckInView.swift
+++ JacketStash_PROD/Core/CheckIn/Views/CheckInView.swift
@@ -30,5 +30,54 @@ struct CheckInView_Previews: PreviewProvider {
     }
 }
 
+<<<<<<< HEAD
 
+=======
+extension CheckInView {
+    
+    var checkInButton : some View {
+        ZStack {
+            Circle()
+                .fill(Color(.systemBlue))
+                .frame(width: 100, height: 100)
+                .scaleEffect(isPressed ? 2 : 0.99)
+                .animation(.easeIn(duration: 1.5), value: isPressed)
+                //.animation(.easeOut(duration: 0.5), value: isPressed)
+            
+            Circle()
+                .fill(Color(.white))
+                .frame(width: 100, height: 100)
+                .scaleEffect(isPressed ? 1.99 : 0.5)
+                .animation(.easeOut(duration: 3), value: isPressed)
+            
+            Button(action: {
+                print("hello")
+            }, label: {})
+            .onLongPressGesture(perform: {
+                print("hello")
+            })
+            .frame(width: 100, height: 100)
+            .background(Color(.systemBlue))
+            .mask(Circle())
+            
+            .pressEvents {
+                withAnimation(.easeIn(duration: 2.75)) {
+                    isPressed = true
+                }
+            
+            } onRelease: {
+                withAnimation(.easeOut(duration: 0.5)) {
+                    isPressed = false
+                }
+            }
+            Text("Check In")
+                .foregroundColor(.white)
+        }
+        
+        .padding(.bottom, 120)
+        
+        
+    }
+}
+>>>>>>> parent of d408034 (Position and text font edits)
 
