9d5b29bb47a3253fa655a68de9a34ae695ab95ef
Adam Yao <adamyao3240@gmail.com>
2022-11-27 01:11:40 -0500

    appicon change and 200 price

--- JacketStash_PROD/Core/Components/Stripe/ViewModel/CheckoutViewModel.swift
+++ JacketStash_PROD/Core/Components/Stripe/ViewModel/CheckoutViewModel.swift
@@ -16,7 +16,7 @@ class CheckoutViewModel : ObservableObject {
     func initiatePayment(withUid uid: String) {
         let data = ["client": "mobile",
                     "mode": "payment",
-                    "amount": "199",
+                    "amount": "200",
                     "currency": "usd"]
         Firestore.firestore().collection("customers").document(uid).collection("checkout_sessions").addDocument(data: data)
     }
